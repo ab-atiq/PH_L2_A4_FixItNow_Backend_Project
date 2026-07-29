b6b23560ace846419865f240461e50132977988a
ab-atiq <abulbasharatiqur@gmail.com>
2026-07-29 20:36:52 +0600

    error

---
+++ src/errors/AppError.ts
@@ -0,0 +1,17 @@
+// src/errors/AppError.ts
+class AppError extends Error {
+  public statusCode: number;
+
+  constructor(statusCode: number, message: string, stack?: string) {
+    super(message);
+    this.statusCode = statusCode;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default AppError;
