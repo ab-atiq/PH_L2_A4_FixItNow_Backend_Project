7629856815cfb218c7332e93ee6344145618ee5a
ab-atiq <abulbasharatiqur@gmail.com>
2026-07-29 20:38:26 +0600

    jwt

---
+++ src/utils/jwtHelper.ts
@@ -0,0 +1,27 @@
+// src/utils/jwtHelper.ts
+import jwt, { JwtPayload, Secret, SignOptions } from "jsonwebtoken";
+import { Role } from "../../generated/prisma/enums";
+
+export type TJwtPayload = {
+  id: string;
+  name: string;
+  email: string;
+  role: Role;
+};
+
+const generateToken = (
+  payload: TJwtPayload,
+  secret: Secret,
+  expiresIn: SignOptions["expiresIn"]
+): string => {
+  return jwt.sign(payload, secret, { expiresIn } as SignOptions);
+};
+
+const verifyToken = (token: string, secret: Secret): TJwtPayload => {
+  return jwt.verify(token, secret) as TJwtPayload;
+};
+
+export const jwtHelper = {
+  generateToken,
+  verifyToken,
+};
