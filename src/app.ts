e283bd9a09e7801d0d694d62cee4f35c6fd0740b
ab-atiq <abulbasharatiqur@gmail.com>
2026-08-07 14:48:32 +0600

    type of request, response

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { type Application, type Request, type Response } from "express";
 import config from "./config";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import { notFound } from "./middlewares/notFound";
